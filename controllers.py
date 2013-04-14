5f041b5ffc63cc5982f65c3acaf7ecab557f71a8
mahwish <mahwishfaryad@yahoo.com>
2013-04-15 02:52:11 +0600

    Create controllers.py
    sir plz check out this code

---
+++ controllers.py
@@ -0,0 +1,80 @@
+from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+
+from pyck.forms import model_form
+from wtforms.widgets.core import Select
+from wtforms import SelectField
+from ..models import DBSession, Info_projects
+    
+from ..forms import ContactForm, NewForm, ItemForm
+    
+from pyck.controllers import CRUDController
+
+class Info_projectsCRUDController(CRUDController):
+    model = Info_projects
+    db_session = DBSession
+
+@view_config(route_name='home', renderer='home.mako')
+def my_view(request):
+    one = None
+    return {'one': one, 'project': 'designer'}
+
+
+@view_config(route_name='contact', renderer="contact.mako")
+def contact_form(request):
+
+    f = ContactForm(request.POST)   # empty form initializes if not a POST request
+
+    if 'POST' == request.method and 'form.submitted' in request.params:
+        if f.validate():
+            #TODO: Do email sending here.
+
+            request.session.flash("Your message has been sent!")
+            return HTTPFound(location=request.route_url('home'))
+
+    return {'contact_form': f}
+
+
+
+
+
+
+@view_config(route_name='new', renderer="new.mako")
+def new_form(request):
+
+    f = NewForm(request.POST)   # empty form initializes if not a POST request
+
+    if 'POST' == request.method and 'form.submitted' in request.params:
+        if f.validate():
+            #TODO: Do email sending here.
+            P = Info_projects()
+        
+            new_form.populate_obj(P)
+            DBSession.add(P)
+
+            request.session.flash("Your message has been sent!")
+            return HTTPFound(location=request.route_url('home'))
+
+    return {'new_form': f}
+
+
+
+
+
+
+
+@view_config(route_name='item', renderer="item.mako")
+def item_form(request):
+
+    f = ItemForm(request.POST)   # empty form initializes if not a POST request
+
+    if 'POST' == request.method and 'form.submitted' in request.params:
+        if f.validate():
+            #TODO: Do email sending here.
+
+            request.session.flash("Your message has been sent!")
+            return HTTPFound(location=request.route_url('home'))
+
+    return {'item_form': f}
+
+
