01f3f22d9a4891b6a33247f7cf1847cc0e4821d4
mahwish <mahwishfaryad@yahoo.com>
2013-04-24 01:39:58 +0600

    Update controllers.py
    problem

--- mitteilender/controllers/controllers.py
+++ mitteilender/controllers/controllers.py
@@ -1,12 +1,18 @@
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
 
-from ..models import (
-    DBSession,
-    )
+from pyck.forms import model_form
+from wtforms.widgets.core import Select
+from wtforms import SelectField
+from ..models import DBSession, Info_projects
+    
+from ..forms import ContactForm, NewForm, ItemForm
+    
+from pyck.controllers import CRUDController
 
-from ..forms import ContactForm
-
+class Info_projectsCRUDController(CRUDController):
+    model = Info_projects
+    db_session = DBSession
 
 @view_config(route_name='home', renderer='home.mako')
 def my_view(request):
@@ -27,3 +33,49 @@ def contact_form(request):
             return HTTPFound(location=request.route_url('home'))
 
     return {'contact_form': f}
+
+
+
+
+
+
+@view_config(route_name='new', renderer="new.mako")
+def new_form(request):
+
+    f = NewForm(request.POST)   # empty form initializes if not a POST request
+
+    if 'POST' == request.method and 'form.submitted' in request.params:
+        if f.validate():
+            #TODO: Do email sending here.
+            P = Info_projects()
+        
+            f.populate_obj(P)
+            DBSession.add(P)
+            categories = DBSession.query(Info_projects.ip_id, Info_projects.name).all()
+
+            request.session.flash("Your message has been sent!")
+            return HTTPFound(location=request.route_url('home'))
+
+    return {'new_form': f}
+
+
+
+
+
+
+
+@view_config(route_name='item', renderer="item.mako")
+def item_form(request):
+
+    f = ItemForm(request.POST)   # empty form initializes if not a POST request
+
+    if 'POST' == request.method and 'form.submitted' in request.params:
+        if f.validate():
+            #TODO: Do email sending here.
+
+            request.session.flash("Your message has been sent!")
+            return HTTPFound(location=request.route_url('home'))
+
+    return {'item_form': f}
+
+
