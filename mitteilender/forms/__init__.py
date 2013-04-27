d2d7a44ab69269eae230e2985ac3866102739b98
mahwish <mahwishfaryad@yahoo.com>
2013-04-27 21:55:26 +0600

    Update __init__.py

--- mitteilender/forms/__init__.py
+++ mitteilender/forms/__init__.py
@@ -1,4 +1,6 @@
 from contact import ContactForm
+from new import NewForm
+from item import ItemForm
 from auth import UserForm, PermissionForm, RoutePermissionForm, LoginForm
 
-__all__ = ['ContactForm', 'UserForm', 'PermissionForm', 'RoutePermissionForm', 'LoginForm']
+__all__ = ['ContactForm','NewForm','ItemForm', 'UserForm', 'PermissionForm', 'RoutePermissionForm', 'LoginForm']
