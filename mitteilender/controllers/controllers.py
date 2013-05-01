6a8e04aadfe886029384925acc539a81fdf181bf
mahwish <mahwishfaryad@yahoo.com>
2013-05-02 03:20:05 +0600

    Update controllers.py

--- mitteilender/controllers/controllers.py
+++ mitteilender/controllers/controllers.py
@@ -39,29 +39,49 @@ def contact_form(request):
 
 
 
-@view_config(route_name='new', renderer="new.mako")
-def new_form(request):
+#@view_config(route_name='new', renderer="new.mako")
+#def new_form(request):
 
-    f = NewForm(request.POST)   # empty form initializes if not a POST request
+ #   f = NewForm(request.POST)   # empty form initializes if not a POST request
 
-    if 'POST' == request.method and 'form.submitted' in request.params:
-        if f.validate():
+  #  if 'POST' == request.method and 'form.submitted' in request.params:
+   #     if f.validate():
             #TODO: Do email sending here.
-            P = Info_projects()
+    #        P = Info_projects()
         
-            f.populate_obj(P)
-            DBSession.add(P)
+     #       f.populate_obj(P)
+      #      DBSession.add(P)
 
-            request.session.flash("Your message has been sent!")
-            return HTTPFound(location=request.route_url('dbshow'))
+       #     request.session.flash("Your message has been sent!")
+        #    return HTTPFound(location=request.route_url('dbshow'))
 
-    return {'new_form': f}
+    #return {'new_form': f}
   
 @view_config(route_name='dbshow', renderer="dbshow.mako")   
 def my_savings(request):
+  
+  
+  
+  
+    var1=request.POST['name1']
+    var2=request.POST['name2']
+    model=Info_projects(name=var1, ip_description=var2)
+    DBSession.add(model)
     acc2 = DBSession.query(Info_projects).all()
     
     return {'acc2':acc2}  
+  
+  
+  
+  
+  
+  
+  
+  
+@view_config(route_name='mahi', renderer="mahi.mako")   
+def my_sav(request):
+  return {}
+  
     
 
 
