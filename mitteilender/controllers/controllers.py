70307e4a9925d53cbd22246418023b225ec8f404
mahwishfaryad <mahwishfaryad@yahoo.com>
2013-11-28 22:26:35 +0500

    3 examples are added in proj

--- mitteilender/controllers/controllers.py
+++ mitteilender/controllers/controllers.py
@@ -104,3 +104,30 @@ def upload_image(request):
 
     return {'project': P, 'image_items': image_items}
 
+@view_config(route_name='upload_file', renderer='upload_file.mako')
+def upload_file(request):
+    project_name = request.matchdict['pname']
+
+    if 'POST' == request.method:
+        item_id = int(request.POST['item_id'])
+        file_data = request.POST['data_file'].file.read()
+        project_item = DBSession.query(project_items).filter_by(pi_id=item_id).first()
+        if not project_item:
+            return HTTPNotFound(message="Project item does not exist")
+
+        current_folder = os.path.dirname(__file__)
+        filename = "../static/uploaded_files/%i.odt" % item_id
+        complete_path = os.path.abspath(os.path.join(current_folder, filename))
+        #print(complete_path)
+        open(complete_path, 'wb').write(file_data)
+        project_item.item_value = 'static/uploaded_files/%i.odt' % item_id
+        request.session.flash("File uploaded!")
+        
+    P = DBSession.query(Info_projects).filter_by(name=project_name).first()
+    if not P:
+        return HTTPNotFound(message="Project %s does not exist" % project_name)
+
+    file_items = DBSession.query(project_items).filter_by(infoproject_id=P.ip_id, item_type='file').order_by(project_items.display_order)
+
+    return {'project': P, 'file_items': file_items}
+
---
+++ mitteilender/templates/upload_file.mako
@@ -0,0 +1,33 @@
+ 
+<%inherit file="base.mako"/>
+
+<div>
+    <h1>Upload File - File Items in Project: ${project.name}</h1>
+    <table>
+        <tr class="tr_heading">
+            <th>Item name</th>
+            <th>Value</th>
+            <th>Parent Item</th>
+            <th>Upload</th>
+            <th>Current File</th>
+        </tr>
+        %for k in file_items:
+            <tr class="${loop.cycle('oddrow', 'evenrow')}">
+                <td>${k.item_name}</td>
+                <td>${k.item_value}</td>
+                <td>${k.parent_item}</td>
+                <td>
+                    <form action="${request.route_url('upload_file', pname=project.name)}" method="POST" enctype="multipart/form-data">
+                        <input type="hidden" name="item_id" value="${k.pi_id}" />
+                        <input type="file" name="data_file" />
+                        <input type="submit" value="upload" />
+                    </form>
+                </td>
+                <td>
+                    <img src="${request.static_url('mitteilender:static/uploaded_files/%i.odt' % k.pi_id)}" height="50" width="50"
+                </td>
+            </tr>
+        %endfor
+        
+    </table>
+</div>
